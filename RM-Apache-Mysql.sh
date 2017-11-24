792f6e4a2f210aa01bc7fac2233c7bde8b6ed200
falceso <falceso@users.noreply.github.com>
2017-11-23 21:53:28 -0800

    Update RM-Apache-Mysql.sh

--- RM-Apache-Mysql.sh
+++ RM-Apache-Mysql.sh
@@ -3,10 +3,10 @@
 #Purpose = Remove Apache and Mysql and replace with better alternatives.
 #Created on 11-22-2017
 #Author = falceso
-#Version 1.3
+#Version 1.4
 sudo service apache2 stop
 sudo apt-get purge apache2 apache2-utils apache2.2-bin apache2-common mysql-y
 sudo apt-get autoremove -y
-sudo apt-get remove apache2* mysql*-y
+sudo apt-get remove apache2* mysql* -y
 sudo apt-get install nginx mariadb-server -y
 
